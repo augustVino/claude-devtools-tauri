8def62a8c5616b5e4984ea32cf177d3d444dd0a5
Vino <zhangyang1@liepin.com>
2026-03-25 22:42:18 +0800

    chore: add tauri-plugin-notification dependency and setup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,6 +21,7 @@ pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_dialog::init())
     .plugin(tauri_plugin_opener::init())
+    .plugin(tauri_plugin_notification::init())
     .manage(app_state.clone())
     .setup(move |app| {
       let state = app_state.clone();
